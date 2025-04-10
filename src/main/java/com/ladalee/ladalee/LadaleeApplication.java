263bd00667a58a18005a8b902e0b9b3be7a45838
advitiya08 <sri.advitiya@gmail.com>
2025-04-10 22:46:17 +0530

    Initial spring project

---
+++ src/main/java/com/ladalee/ladalee/LadaleeApplication.java
@@ -0,0 +1,13 @@
+package com.ladalee.ladalee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadaleeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadaleeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ladalee/ladalee/LadaleeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ladalee.ladalee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadaleeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
